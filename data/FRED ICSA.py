fbcee4644a30ba0c96f52dbf9b1d1e283610c231
Trang Ly <102381366+tly23@users.noreply.github.com>
2025-10-23 21:14:40 -0400

    Add files via upload
    code to retrieve unemployment data from FRED API

---
+++ data/FRED ICSA.py
@@ -0,0 +1,45 @@
+from fredapi import Fred
+import pandas as pd
+
+fred = Fred(api_key='API_KEY_HERE')
+
+state_mapping = {
+    'AL': 'Alabama', 'AK': 'Alaska', 'AZ': 'Arizona', 'AR': 'Arkansas',
+    'CA': 'California', 'CO': 'Colorado', 'CT': 'Connecticut', 'DE': 'Delaware',
+    'FL': 'Florida', 'GA': 'Georgia', 'HI': 'Hawaii', 'ID': 'Idaho',
+    'IL': 'Illinois', 'IN': 'Indiana', 'IA': 'Iowa', 'KS': 'Kansas',
+    'KY': 'Kentucky', 'LA': 'Louisiana', 'ME': 'Maine', 'MD': 'Maryland',
+    'MA': 'Massachusetts', 'MI': 'Michigan', 'MN': 'Minnesota', 'MS': 'Mississippi',
+    'MO': 'Missouri', 'MT': 'Montana', 'NE': 'Nebraska', 'NV': 'Nevada',
+    'NH': 'New Hampshire', 'NJ': 'New Jersey', 'NM': 'New Mexico', 'NY': 'New York',
+    'NC': 'North Carolina', 'ND': 'North Dakota', 'OH': 'Ohio', 'OK': 'Oklahoma',
+    'OR': 'Oregon', 'PA': 'Pennsylvania', 'RI': 'Rhode Island', 'SC': 'South Carolina',
+    'SD': 'South Dakota', 'TN': 'Tennessee', 'TX': 'Texas', 'UT': 'Utah',
+    'VT': 'Vermont', 'VA': 'Virginia', 'WA': 'Washington', 'WV': 'West Virginia',
+    'WI': 'Wisconsin', 'WY': 'Wyoming', 'PR': 'Puerto Rico', 'VI': 'Virgin Islands'
+}
+
+states = list(state_mapping.keys())
+
+# Download initial state-level claims (ICSA = Initial Claims by State)
+print('Downloading Initial Claims data...')
+state_claims = {}
+for state in states:
+    series_id = f'{state}ICLAIMS'
+    try:
+        state_claims[state] = fred.get_series(series_id,
+                                                observation_start='2020-01-01',
+                                                observation_end='2022-12-20')
+        print(f'Successfully retrieved data for {state}')
+    except:
+        print(f'Failed to retrieve data for {state}')
+
+# Combine all state data into a single DataFrame
+df_state_claims = pd.DataFrame(state_claims)
+df_state_claims.to_csv('state_unemployment_claims.csv')
+
+# Melt the DataFrame to long format
+df_melted = df_state_claims.reset_index().melt(id_vars='index', var_name='Province_State', value_name='Initial_Claims')
+
+df_melted.rename(columns={'index': 'Date'}, inplace=True)
+df_melted.to_csv('state_unemployment_claims_long.csv', index=False)
