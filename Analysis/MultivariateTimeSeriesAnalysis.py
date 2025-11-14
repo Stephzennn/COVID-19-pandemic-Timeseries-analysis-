a5c8643643b6b4c7037b3455aba632d4d42b9aaf
Stephzennn <estifanosSNzen@outlook.com>
2025-11-14 12:58:14 -0500

    Clean and add some files

---
+++ Analysis/MultivariateTimeSeriesAnalysis.py
@@ -0,0 +1,19 @@
+
+
+import os
+
+os.getcwd()
+
+import os, sys
+
+# Path to parent folder
+parent = os.path.dirname(os.getcwd())
+data_path = os.path.join(parent, "data")
+data_path
+
+# Add to path if not already present
+if data_path not in sys.path:
+
+    sys.path.append(data_path)
+
+from data.GetCombinedData import getCombinedData
